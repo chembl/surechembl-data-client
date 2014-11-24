7276e937e2e104a069a83a37de17b82e0908430c
James Siddle <j.siddle@digital-science.com>
2014-11-24 15:41:52 +0000

    Complete filtering and download steps

--- scripts/ftp_test.py
+++ scripts/ftp_test.py
@@ -1,28 +1,38 @@
 import unittest
 from mock import MagicMock, ANY
+from mock import call
 import datetime
 import ftplib
 
 from new_file_reader import NewFileReader
 
-class FTPTests(unittest.TestCase):
-
-    file_chunk1 = '''\
+file_chunk1 = '''\
 path/to/file/1
 path/to/file/2
 path/to/fi'''
-    file_chunk2 = '''\
+file_chunk2 = '''\
 le/3
 longer/path/to/different/file/A
 longer/path/to/differe'''
-    file_chunk3 = '''\
+file_chunk3 = '''\
 nt/file/B
 longer/path/to/different/file/C'''
+
+def chunk_writer(*args, **kwargs):
+    writer_func = args[1]
+    writer_func(file_chunk1)
+    writer_func(file_chunk2)
+    writer_func(file_chunk3)
+
+
+
+class FTPTests(unittest.TestCase):
 
     def setUp(self):
         self.ftp = ftplib.FTP()
         self.ftp.cwd        = MagicMock(return_value=None)
         self.ftp.retrbinary = MagicMock(return_value=None)
+        self.ftp.retrbinary.side_effect = chunk_writer
         self.reader = NewFileReader(self.ftp)
 
     def test_create_new_file_reader(self):
@@ -34,13 +44,6 @@ longer/path/to/different/file/C'''
 
     def test_get_new_files(self):
 
-        def chunk_writer(*args, **kwargs):
-            writer_func = args[1]
-            writer_func(self.file_chunk1)
-            writer_func(self.file_chunk2)
-            writer_func(self.file_chunk3)
-
-        self.ftp.retrbinary.side_effect = chunk_writer
         files = self.reader.new_files( datetime.date(2012,10,3) )
 
         self.ftp.cwd.assert_called_with( "data/external/frontfile/2012/10/03" )
@@ -56,6 +59,39 @@ longer/path/to/different/file/C'''
              'longer/path/to/different/file/C'
              ])
 
+    def test_get_download_list(self):
+        self.verify_downloads([],[])
+        self.verify_downloads(
+            ['path/orig.chemicals.tsv', 'path/orig.biblio.json'],
+            ['path/orig.biblio.json', 'path/orig.chemicals.tsv'])
+        self.verify_downloads(
+            ['path/new_supp2.chemicals.tsv'],
+            ['path/new.biblio.json', 'path/new_supp2.chemicals.tsv'])
+        self.verify_downloads(
+            ['chemicals.json'],
+            [])
+        self.verify_downloads(
+            ['path/orig.chemicals.tsv', 'path/orig.biblio.json', 'path/new_supp2.chemicals.tsv', 'other/file'],
+            ['path/new.biblio.json', 'path/orig.biblio.json', 'path/new_supp2.chemicals.tsv', 'path/orig.chemicals.tsv'])
+
+
+    def verify_downloads(self, input_list, expected):
+        actual = self.reader.select_downloads(input_list)
+        self.failUnlessEqual(expected, actual)
+
+
+    def test_get_files(self):
+        self.reader.read_files(['path/one/bib.dat','path/two/chem.dat'], '/tmp/')
+
+        calls = [call('/'),
+                 call('path/one/'),
+                 call('/'),
+                 call('path/two/')]
+        self.ftp.cwd.assert_has_calls(calls, any_order=False)
+
+        calls = [call("RETR bib.dat", ANY),
+                 call("RETR chem.dat", ANY)]
+        self.ftp.retrbinary.assert_has_calls(calls, any_order=False)
 
 
 def main():
--- scripts/new_file_reader.py
+++ scripts/new_file_reader.py
@@ -2,6 +2,7 @@
 
 import sys
 import ftplib
+import re
 from datetime import date
 
 class NewFileReader:
@@ -13,6 +14,8 @@ class NewFileReader:
         '''
 
         self.ftp = ftp
+        self.supp_regex = re.compile('_supp[0-9]+.chemicals.tsv')
+
 
     def new_files(self, from_date):
         '''
@@ -38,14 +41,56 @@ class NewFileReader:
 
         return content.split('\n')
 
+    def select_downloads(self, file_list):
+
+        bibl_files = set()
+        chem_files = set()
+
+        for file in file_list:
+            if file.endswith('.biblio.json'):
+                bibl_files.add(file)
+            elif file.endswith('.chemicals.tsv'):
+                chem_files.add(file)
+
+        supp_chems = filter( lambda f: self.supp_regex.search(f), file_list )
+
+        for sc in supp_chems:
+            bibl_files.add( self.supp_regex.sub('.biblio.json', sc) )
+
+        return sorted(bibl_files) + sorted(chem_files)
+
+
+    def read_files(self,file_list,target_dir):
+
+        for file_path in file_list:
+
+            matched = re.match(r'(.*/)([^/]+$)', file_path)
+            path = matched.group(1)
+            file = matched.group(2)
+
+            fhandle = open("{0}/{1}".format(target_dir,file), 'wb')
+
+            self.ftp.cwd( '/' )
+            self.ftp.cwd( path )
+            self.ftp.retrbinary("RETR " + file, fhandle.write)
+
+            fhandle.close()
+
+
 
 def main():
     '''Default behaviour for the file reader: get today's file list, print to standard out'''
     ftp = ftplib.FTP('ftp-private.ebi.ac.uk', sys.argv[1], sys.argv[2])
-    reader = NewFileReader(ftp)
-    file_list = reader.new_files( date.today() )
-    for file_name in file_list:
-        print file_name
+    reader        = NewFileReader(ftp)
+    file_list     = reader.new_files( date.today() )
+    download_list = reader.select_downloads( file_list )
+
+    reader.read_files( download_list, '/tmp' )
+
 
 if __name__ == '__main__':
     main()
+
+
+# TODO Add command line arg handling
+# TODO Strings into constants
