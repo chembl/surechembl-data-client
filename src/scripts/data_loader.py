e5b4b70a756ed7886e009d14b8f43cc0a24719ca
James Siddle <j.siddle@digital-science.com>
2014-11-28 11:57:31 +0000

    First pass at full document loading

--- src/scripts/data_loader.py
+++ src/scripts/data_loader.py
@@ -1,22 +1,27 @@
 import codecs
 import json
 from datetime import datetime
-from sqlalchemy import MetaData, Table, Column, Sequence, Integer, String, SmallInteger, Date
+from sqlalchemy import MetaData, Table, ForeignKey, Column, Sequence, Integer, String, SmallInteger, Date, Text
 
+class DocumentClass:
 
-def chunks(l, n):
-    """ Yield successive n-sized chunks from l. Via Stack Overflow.
-    """
-    for i in xrange(0, len(l), n):
-        yield l[i:i+n]
+    IPC  = 1
+    ECLA = 2
+    IPCR = 3
+    CPC  = 4
 
-
+    bib_dict = {
+        'ipc'  : IPC,
+        'ecla' : ECLA,
+        'ipcr' : IPCR,
+        'cpc'  : CPC }
 
 class DataLoader:
-
     def __init__(self, db):
         self.db = db
         self.metadata = MetaData()
+
+        # TODO field sizes asserted - all
         self.docs = Table('schembl_document', self.metadata,
                      Column('id',                Integer,       Sequence('schembl_document_id'), primary_key=True),
                      Column('scpn',              String(50),    unique=True),
@@ -24,7 +29,18 @@ class DataLoader:
                      Column('life_sci_relevant', SmallInteger()),
                      Column('family_id',         Integer))
 
-        # TODO field sizes asserted
+        # TODO FK and Nullable tested
+        self.titles = Table('schembl_document_title', self.metadata,
+                     Column('schembl_doc_id',    Integer,       ForeignKey('schembl_document.id'), primary_key=True),
+                     Column('lang',              String(10),    primary_key=True),
+                     Column('text',              Text()))
+
+        # TODO FK and Nullable tested
+        self.classes = Table('schembl_document_class', self.metadata,
+                     Column('schembl_doc_id',    Integer,        ForeignKey('schembl_document.id'), primary_key=True),
+                     Column('class',             String(100),    primary_key=True),
+                     Column('system',            SmallInteger(), primary_key=True))
+
 
 
     def db_metadata(self):
@@ -38,45 +54,75 @@ class DataLoader:
 
         conn = self.db.connect()
 
-        for chunk in chunks(biblio, 5):
+        # TODO retrieve known document IDs (configurable)
 
-            transaction = conn.begin()
+        for chunk in chunks(biblio, 1000):
+
 
             # TODO Multiple inserts?
             for bib in chunk:
 
-                # TODO empty values rejected (or accepted)
+                transaction = conn.begin()
 
-                # TODO improve handling of default list extraction
-                pubdate = datetime.strptime( bib['pubdate'][0], '%Y%m%d')
+                # TODO missing / empty values rejected (or explicitly allowed)
+
+                pubdate = datetime.strptime( bib_scalar( bib,'pubdate'), '%Y%m%d')
 
                 record = dict(
-                    scpn              = bib['pubnumber'][0],
+                    scpn              = bib_scalar(bib, 'pubnumber'),
                     published         = pubdate,
-                    family_id         = bib['family_id'][0],
+                    family_id         = bib_scalar(bib, 'family_id'),
                     life_sci_relevant = 1 )
 
                 # TODO duplicate SCPN
                 # TODO life science relevant function
 
-
                 ins = self.docs.insert().values( record )
-
                 result = conn.execute(ins)
 
-                print result.inserted_primary_key
-
-                # TODO retrieve full set of document IDs (pre-load?)
-                # TODO param to disable document ID query
-
-                # TODO titles and classifications inserted
+                doc_id = result.inserted_primary_key[0] # Single PK
 
 
-            # TODO appropriate transaction scope
-            transaction.commit()
+                # TODO missing titles handled
+                titles = bib['title']
+                for i, title_lang in enumerate( bib['title_lang'] ):
+                    title = titles[i]
+
+                    record = dict(
+                        schembl_doc_id = doc_id,
+                        lang           = title_lang,
+                        text           = title)
+
+                    ins = self.titles.insert().values( record )
+                    result = conn.execute(ins)
+
+                # TODO missing classifications handled
+                for system_key in ['ipc','ecla','ipcr','cpc']:
+
+                    for classif in bib[system_key]:
+
+                        record = {
+                            'schembl_doc_id': doc_id,
+                            'class'         : classif,
+                            'system'        : DocumentClass.bib_dict[system_key] }
+
+                        ins = self.classes.insert().values( record )
+                        result = conn.execute(ins)
+
+                transaction.commit()
 
         conn.close()
-        codecs.close()
+        input_file.close()
+
+
+
+def chunks(l, n):
+    ''' Yield successive n-sized chunks from l. Via Stack Overflow.'''
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+def bib_scalar(biblio, key):
+    return biblio[key][0]
 
 
 
--- src/tests/data_load_test.py
+++ src/tests/data_load_test.py
@@ -1,14 +1,20 @@
+# -*- coding: UTF-8 -*-
+
 import unittest
 from datetime import date
 from sqlalchemy import create_engine, select, Column, Integer, Sequence
 
-from src.scripts.data_loader import DataLoader
+from src.scripts.data_loader import DataLoader, DocumentClass
 
 class DataLoaderTests(unittest.TestCase):
 
     def setUp(self):
         self.db = create_engine('sqlite:///:memory:', echo=False)
+
+        # Create the object under test, use it to create the schema
         self.loader = DataLoader(self.db)
+        self.metadata = self.loader.db_metadata()
+        self.metadata.create_all(self.db)
 
     def test_create_doc_loader(self):
         self.failUnless( isinstance(self.loader, DataLoader) )
@@ -18,37 +24,98 @@ class DataLoaderTests(unittest.TestCase):
 
         result = self.load_n_query('data/biblio_single_row.json')
         row = result.fetchone()
-
-        self.failUnless( row['id'] == 1 )
-        self.failUnless( row['scpn'] == 'WO-2013127697-A1' )
-        self.failUnless( row['published'] == date(2013,9,6) )
-        self.failUnless( row['family_id'] == 47747634 )
-        self.failUnless( row['life_sci_relevant'] == 1 )
+        self.verify_doc(row, (1,'WO-2013127697-A1',date(2013,9,6),1,47747634))
 
     def test_write_docs_many(self):
 
         result = self.load_n_query('data/biblio_all_round.json')
         rows = result.fetchall()
         self.failUnlessEqual( 25, len(rows) )
-        self.failUnlessEqual( (1,'WO-2013127697-A1',date(2013,9,6),1,47747634), rows[0] )
-        self.failUnlessEqual( (2,'WO-2013127698-A1',date(2013,9,6),1,47748611), rows[1] )
-        self.failUnlessEqual( (25,'WO-2013189394-A2',date(2013,12,27),1,49769540), rows[24] )
+        self.verify_doc( rows[0], (1,'WO-2013127697-A1',date(2013,9,6),1,47747634) )
+        self.verify_doc( rows[1], (2,'WO-2013127698-A1',date(2013,9,6),1,47748611) )
+        self.verify_doc( rows[24], (25,'WO-2013189394-A2',date(2013,12,27),1,49769540) )
 
     def test_sequence_definitions(self):
         mdata = self.loader.db_metadata()
         self.failUnlessEqual( 'schembl_document_id', mdata.tables['schembl_document'].c.id.default.name )
 
+    def test_titles(self):
+        result = self.load_n_query('data/biblio_all_round.json', 'schembl_document_title')
+        rows = result.fetchall()
+        self.failUnlessEqual( 62, len(rows) )
+        self.verify_title( rows[0],  (1, "DE", u"VERWENDUNG EINES LATENTREAKTIVEN KLEBEFILMS ZUR VERKLEBUNG VON ELOXIERTEM ALUMINIUM MIT KUNSTSTOFF") )
+        self.verify_title( rows[1],  (1, "EN", u"USE OF A LATENTLY REACTIVE ADHESIVE FILM FOR ADHESIVE BONDING OF ELOXATED ALUMINIUM TO PLASTIC") )
+        self.verify_title( rows[2],  (1, "FR", u"UTILISATION D'UN FILM ADHÉSIF À RÉACTIVITÉ LATENTE POUR LE COLLAGE DE PLASTIQUE SUR DE L'ALUMINIUM ANODISÉ") )
+        self.verify_title( rows[56], (24,"EN", u"TRAFFIC SHAPING DRIVE METHOD AND DRIVER") )
+        self.verify_title( rows[57], (24,"FR", u"PROCÉDÉ DE PILOTAGE DE MISE EN FORME DE TRAFIC ET ORGANE PILOTAGE") )
+        self.verify_title( rows[58], (24,"ZH", u"一种流量整形的驱动方法及驱动器") )
+        self.verify_title( rows[59], (25,"EN", u"RESOURCE INFORMATION ACQUISITION METHOD, SYSTEM AND DEVICE FOR INTERNET OF THINGS TERMINAL DEVICE") )
+        self.verify_title( rows[60], (25,"FR", u"PROCÉDÉ, SYSTÈME ET DISPOSITIF D'ACQUISITION D'INFORMATIONS SUR LES RESSOURCES, POUR DISPOSITIF TERMINAL DE L'INTERNET DES OBJETS") )
+        self.verify_title( rows[61], (25,"ZH", u"一种物联网终端设备的资源信息获取方法、系统及设备") )
 
-    def load_n_query(self, data_file):
+    def test_classifications_simple(self):
+        result = self.load_n_query('data/biblio_single_row.json', 'schembl_document_class')
+        rows = result.fetchall()
+        self.verify_class( rows[0], (1, "B29C", DocumentClass.IPC) )
 
-        metadata = self.loader.db_metadata()
-        metadata.create_all(self.db)
+    def test_classifications_all(self):
+        self.load_n_query('data/biblio_all_round.json')
+
+        # Check a document with all classifications
+        self.verify_classes( 1, DocumentClass.IPC,  ["B29C"])
+        self.verify_classes( 1, DocumentClass.ECLA, ["B29C"])
+        self.verify_classes( 1, DocumentClass.IPCR, ["B29C 65/50","B32B 37/12","B32B 7/12","C08K 5/29","C08K 5/32","C09J 7/00","C09J 7/02","C09J 7/04"])
+        self.verify_classes( 1, DocumentClass.CPC,  ["B29C 65/4835","B29C 65/5057","B29C 66/7422","B32B 2038/042","B32B 2309/02","B32B 2309/04","B32B 2309/12","B32B 2457/00","B32B 37/0046","B32B 37/1207","B32B 38/1841","B32B 7/12","C08J 5/12","C09J 2205/102","C09J 2475/00","C09J 5/00","C09J 7/00","C09J 7/0203","C09J 7/043"])
+
+        # Check documents with none / some
+        self.verify_classes( 2, DocumentClass.IPC,  [])
+        self.verify_classes( 2, DocumentClass.ECLA, [])
+        self.verify_classes( 2, DocumentClass.IPCR, [])
+        self.verify_classes( 2, DocumentClass.CPC,  [])
+
+        self.verify_classes( 25, DocumentClass.IPC,  [])
+        self.verify_classes( 25, DocumentClass.ECLA, [])
+        self.verify_classes( 25, DocumentClass.IPCR, ["H04L 29/08"])
+        self.verify_classes( 25, DocumentClass.CPC,  ["H04L 29/08"])
+
+    def load_n_query(self, data_file, table='schembl_document', where_clause=(True == True), order_by_clause=('')):
 
         self.loader.load( data_file )
-        s = select([metadata.tables['schembl_document']])
+        s = select( [self.metadata.tables[table]] ).where( where_clause ).order_by( order_by_clause )
 
         result = self.db.execute(s)
         return result
+
+    def verify_doc(self, row, expected):
+        self.failUnlessEqual( expected[0], row['id'] )
+        self.failUnlessEqual( expected[1], row['scpn'] )
+        self.failUnlessEqual( expected[2], row['published'] )
+        self.failUnlessEqual( expected[3], row['life_sci_relevant'] )
+        self.failUnlessEqual( expected[4], row['family_id'] )
+
+    def verify_title(self, row, expected):
+        self.failUnlessEqual( expected[0], row['schembl_doc_id'] )
+        self.failUnlessEqual( expected[1], row['lang'] )
+        self.failUnlessEqual( expected[2], row['text'] )
+
+    def verify_classes(self, doc, system, classes):
+        classif_table = self.metadata.tables['schembl_document_class']
+
+        s = select( [classif_table] )\
+            .where( classif_table.c.schembl_doc_id == doc )\
+            .where( classif_table.c.system == system )
+        rows = self.db.execute(s).fetchall()
+
+        self.failUnlessEqual(len(classes), len(rows))
+
+        for i,row in enumerate(rows):
+            self.verify_class(row, (doc, classes[i], system) )
+
+    def verify_class(self, row, expected):
+        self.failUnlessEqual( expected[0], row['schembl_doc_id'] )
+        self.failUnlessEqual( expected[1], row['class'] )
+        self.failUnlessEqual( expected[2], row['system'] )
+
 
 
 def main():
