ce5b101506a710d899894c3a77ab9755e45b229c
James Siddle <j.siddle@digital-science.com>
2015-02-12 12:08:37 +0000

    Add retry helper

---
+++ src/scripts/helper_funcs.py
@@ -0,0 +1,34 @@
+
+import logging
+from time import sleep 
+
+logger = logging.getLogger(__name__)
+
+def retry(max_attempts, func, parms, sleep_secs=0):
+  """
+  Provides a general purpose retry mechanism for Exception throwing functions
+  """
+
+	complete = False
+	attempts = 0
+
+	# Main retry loop
+	while attempts < max_attempts and not complete:
+
+		attempts += 1
+
+		try:
+			func(*parms)			
+			complete = True
+		except Exception, e:
+			last_exc = e
+			logger.warn( "Exception caught in retry block: [{}]".format(e) )
+			logger.warn( "Sleeping for {} seconds".format(sleep_secs) )
+			sleep(sleep_secs)
+
+	# Post processing - error logging, and rethrow exception if needed
+	if complete:
+		logger.debug( "Retry block succeeded after {} attempts".format(attempts) )
+	else:
+		logger.error( "Retry block unable to complete after {} attempts".format(attempts) )
+		raise last_exc
---
+++ src/tests/helper_test.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import unittest
+import shutil
+import datetime
+import ftplib
+
+from mock import MagicMock, ANY
+from mock import call
+
+from src.scripts.helper_funcs import retry
+
+class HelperTests(unittest.TestCase):
+
+    def setUp(self):
+    	self.f = MagicMock( return_value=None )
+    	self.fparms = ['parm1', 2, True]
+
+    def test_retry_success(self):
+        retry( 5, self.f, self.fparms )
+        self.f.assert_called_with( 'parm1',2,True )
+
+    def test_retry_failure_basic(self):
+    	self.verify_retry_failures(5,2,0)
+
+    def test_retry_failure_lots(self):
+    	self.verify_retry_failures(10,9,0)
+
+    def test_retry_failure_sleep(self):
+    	self.verify_retry_failures(5,3,1)
+
+    def verify_retry_failures(self, retry_count, fail_count, sleep_time):
+    	self.f.side_effect = ([ KeyError('somekey')]*fail_count) + [ None ]
+        retry( retry_count, self.f, self.fparms, sleep_secs=sleep_time )
+        
+        self.f.assert_called_with( 'parm1',2,True )
+        self.assertTrue(self.f.call_count == fail_count + 1)
+
+    def test_total_failure_exception(self):
+    	self.f.side_effect = Exception('some persistent error')
+
+    	try:
+        	retry( 5, self.f, self.fparms )
+        	self.fail("Exception expected")
+    	except Exception, e:
+    		self.assertEqual( 'some persistent error', str(e) )
+	        self.f.assert_called_with( 'parm1',2,True )
+	        self.assertEqual(5, self.f.call_count)   
+        
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
