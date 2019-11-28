e4a7d56023cfcad80c5a07fd48a1af8d700b25f8
Ric <ricartomojo@gmail.com>
2019-11-28 15:31:50 +0000

    Changed base docker image for oracle container

--- Docker/oracledb/surechembl.sql
+++ Docker/oracledb/surechembl.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE schembl_document (
+CREATE TABLE sc_client.schembl_document (
   id INTEGER NOT NULL,
   scpn VARCHAR2(50) NOT NULL,
   published DATE NULL,
@@ -8,22 +8,22 @@ CREATE TABLE schembl_document (
   family_id INTEGER NULL,
   PRIMARY KEY (id));
 
-CREATE UNIQUE INDEX scpn_UNIQUE ON schembl_document (scpn ASC);
+CREATE UNIQUE INDEX sc_client.scpn_UNIQUE ON sc_client.schembl_document (scpn ASC);
 
-CREATE SEQUENCE schembl_document_id;
+CREATE SEQUENCE sc_client.schembl_document_id;
 
 -- -----------------------------------------------------
 -- Table schembl_document_class
 -- -----------------------------------------------------
 
-CREATE TABLE schembl_document_class (
+CREATE TABLE sc_client.schembl_document_class (
   schembl_doc_id INTEGER NOT NULL,
   class VARCHAR2(100) NOT NULL,
   system SMALLINT NOT NULL,
   PRIMARY KEY (schembl_doc_id, class, system),
   CONSTRAINT fk_docclass_to_doc
     FOREIGN KEY (schembl_doc_id)
-    REFERENCES schembl_document (id));
+    REFERENCES sc_client.schembl_document (id));
 
 
 -- -----------------------------------------------------
@@ -32,20 +32,20 @@ CREATE TABLE schembl_document_class (
 
 -- Oracle: Change 'text' column type to CLOB
 
-CREATE TABLE schembl_document_title (
+CREATE TABLE sc_client.schembl_document_title (
   schembl_doc_id INTEGER NOT NULL,
   lang VARCHAR2(10) NOT NULL,
   text CLOB NULL,
   PRIMARY KEY (schembl_doc_id, lang),
   CONSTRAINT fk_doctitle_to_doc
     FOREIGN KEY (schembl_doc_id)
-    REFERENCES schembl_document (id));
+    REFERENCES sc_client.schembl_document (id));
 
 -- -----------------------------------------------------
 -- Table schembl_chemical
 -- -----------------------------------------------------
 
-CREATE TABLE schembl_chemical (
+CREATE TABLE sc_client.schembl_chemical (
   id INTEGER NOT NULL,
   mol_weight FLOAT NULL,
   logp FLOAT NULL,
@@ -66,7 +66,7 @@ CREATE TABLE schembl_chemical (
 
 -- Oracle: Change 'smiles' and 'std_inchi' column type to CLOB
 
-CREATE TABLE schembl_chemical_structure (
+CREATE TABLE sc_client.schembl_chemical_structure (
   schembl_chem_id INTEGER NOT NULL,
   smiles CLOB NULL,
   std_inchi CLOB NULL,
@@ -74,14 +74,14 @@ CREATE TABLE schembl_chemical_structure (
   PRIMARY KEY (schembl_chem_id),
   CONSTRAINT fk_chemstruct_to_chem
     FOREIGN KEY (schembl_chem_id)
-    REFERENCES schembl_chemical (id));
+    REFERENCES sc_client.schembl_chemical (id));
 
 
 -- -----------------------------------------------------
 -- Table schembl_document_chemistry
 -- -----------------------------------------------------
 
-CREATE TABLE schembl_document_chemistry (
+CREATE TABLE sc_client.schembl_document_chemistry (
   schembl_doc_id INTEGER NOT NULL,
   schembl_chem_id INTEGER NOT NULL,
   field SMALLINT NOT NULL,
@@ -89,13 +89,13 @@ CREATE TABLE schembl_document_chemistry (
   PRIMARY KEY (schembl_doc_id, schembl_chem_id, field),
   CONSTRAINT fk_docchem_to_doc
     FOREIGN KEY (schembl_doc_id)
-    REFERENCES schembl_document (id),
+    REFERENCES sc_client.schembl_document (id),
   CONSTRAINT fk_docchem_to_chem
     FOREIGN KEY (schembl_chem_id)
-    REFERENCES schembl_chemical (id));
+    REFERENCES sc_client.schembl_chemical (id));
 
-CREATE INDEX fk_docchem_docid_idx ON schembl_document_chemistry (schembl_doc_id ASC);
+CREATE INDEX fk_docchem_docid_idx ON sc_client.schembl_document_chemistry (schembl_doc_id ASC);
 
-CREATE INDEX fk_docchem_chemid_idx ON schembl_document_chemistry (schembl_chem_id ASC);
+CREATE INDEX fk_docchem_chemid_idx ON sc_client.schembl_document_chemistry (schembl_chem_id ASC);
 
 exit;
